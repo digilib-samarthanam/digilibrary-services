5973b0505845e7c1b639d6c4048e7b96f05fa3c5
Ruta Chaudhari <3488572-rutachaudhari@users.noreply.gitlab.com>
2020-09-29 17:32:24 +0530

    CORS

--- src/main/java/com/samarthanam/digitallibrary/controller/PdfRenderController.java
+++ src/main/java/com/samarthanam/digitallibrary/controller/PdfRenderController.java
@@ -5,6 +5,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.*;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PdfRenderController {
 
     @GetMapping(value = "digital_library/v1/pdfs/{pdf_book_id}" ,  produces = "application/pdf")
