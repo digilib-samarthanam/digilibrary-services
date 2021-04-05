ba8b128293f89ecbf3c254dd45cf7cf7c7e54eed
Satyendra Kumar <satyendra.kumar@target.com>
2021-04-05 11:57:25 +0530

    fix for book type value - 'Audio Book' / 'PDF'

--- src/main/java/com/samarthanam/digitallibrary/constant/BookType.java
+++ src/main/java/com/samarthanam/digitallibrary/constant/BookType.java
@@ -1,17 +1,24 @@
 package com.samarthanam.digitallibrary.constant;
 
 public enum BookType {
-    PDF(".pdf"),
-    AUDIO_BOOK(".mp3");
+    PDF("PDF", ".pdf"),
+    AUDIO_BOOK("Audio Book", ".mp3");
 
+    private String value;
     private String fileExtension;
 
-    BookType (String fileExtension) {
+    BookType (String value, String fileExtension) {
+        this.value = value;
         this.fileExtension = fileExtension;
     }
 
     public String fileNameWithExtension(String fileName) {
         return fileName + fileExtension;
+    }
+
+    @Override
+    public String toString() {
+        return value;
     }
 
 }
--- src/main/java/com/samarthanam/digitallibrary/service/mapper/BooksMapper.java
+++ src/main/java/com/samarthanam/digitallibrary/service/mapper/BooksMapper.java
@@ -9,19 +9,18 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.NullValueCheckStrategy;
 
-import java.util.List;
-
 @Mapper(nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
 public interface BooksMapper {
 
     @Mapping(target = "author", expression = "java(book.getAuthor().getFirstName() + \" \" + book.getAuthor().getLastName())")
     @Mapping(target = "category", source = "category.categoryName")
-    @Mapping(target = "bookType", source = "bookTypeFormat.bookTypeDescription")
+    @Mapping(target = "bookType", expression = "java(book.getBookTypeFormat().getBookTypeDescription().toString())")
     @Mapping(target = "thumbnailUrl", ignore = true)
     BookResponse map(Book book);
 
     BookActivityStatus map(UserActivityHistory userActivityHistory);
 
     BookActivityStatus map(UserBookmarks userBookmarks);
+
 
 }
