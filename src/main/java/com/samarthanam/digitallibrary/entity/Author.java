0582825bb45989c68f5f67f07efbfe0bdd08c865
Satyendra Kumar <satyendra.kumar@target.com>
2021-05-30 23:51:54 +0530

    Added seq for author id and query to search by author name

--- src/main/java/com/samarthanam/digitallibrary/entity/Author.java
+++ src/main/java/com/samarthanam/digitallibrary/entity/Author.java
@@ -1,25 +1,26 @@
 package com.samarthanam.digitallibrary.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "author")
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Author implements Serializable {
 
     @Id
     @Column(name = "author_id")
+    @SequenceGenerator(name = "seq", sequenceName = "author_id_seq", allocationSize=1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
     private Integer authorId;
 
     @Column(name = "name")
